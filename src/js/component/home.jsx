f5e93692681d0326cc09c419d459e7dbbd3f723d
Cesar Amaya Colella <cesar.rafael.gep@gmail.com>
2024-07-04 00:10:54 +0000

    todo list app using react basic structure and behavior by cesaramcolson

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,9 +1,46 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Home = () => {
+	const [ inputValue, setInputValue ] = useState("")
+	const [ items, setItems ] = useState([])
 	return (
-		<div className="text-center">
-			<h1>To Do List</h1>
+		<div className="container">
+			<h1>Morning todos</h1>
+			<ul>
+				<li><input 
+						type="text" 
+						onChange={(e) => setInputValue(e.target.value)}
+						value={inputValue}
+						onKeyDown={(e) => {
+							if (e.key === 'Enter') {
+							  setItems([...items, inputValue]);
+							  setInputValue('');
+							}
+						  }}
+						placeholder="Add a new task">
+					</input></li>
+					{items.map((item, index) => (
+						<li>
+							<span>{item}</span>
+							<div className="icon">
+								<i 
+									className="fa-solid fa-x" 
+									onClick={() => setItems(
+										items.filter(
+											(t, currentIndex) => 
+												index != currentIndex
+										)
+									)}>
+								</i></div>
+						</li>
+					))}
+			</ul>
+			<div><p>
+					{items.length === 0
+            		? "No pending tasks"
+            		: `${items.length} pending 
+					${items.length === 1 ? "task" : "tasks"}`}
+				</p></div>
 		</div>
 	);
 };
